56bd4951d4005e9d28b0d4d780906acda704fc78
CruzJulian <cruz@chocolatron.cruz>
2024-02-28 12:55:44 -0500

    Cambios en habilidades, hoja de vida corta y retirar correos electrónicos de investigadores en la sección de asesorías.

--- _run_all.sh
+++ _run_all.sh
@@ -4,5 +4,9 @@ cp Cruz-Julian_CV-completa.pdf _other/
 
 mv Cruz-Julian_CV-completa.pdf _esp-input/
 
+quarto render _hv-corta.qmd
+
+mv Cruz-Julian_CV-corta.pdf _other/
+
 quarto render _esp-input/
 
